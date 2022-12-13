a686f0a0a75659981c221c5bbc5bd98ec0782b58
Maryna Kanunnykova <Marunabjnd@gmail.com>
2022-12-13 18:17:19 +0200

    fix git

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
 
+const text = "HelloWorld!"
+const elem = (
+  <div>
+  <h1> {text}</h1>
+  <input type="text" />
+  <button>Кнопка</button>
+  <button>вторая   кнопка</button>
+  </div>
+  
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
